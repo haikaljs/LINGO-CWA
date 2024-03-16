ff343ffdd53e82c2414ed29a8f7e86c39bf8d964
haikaljs <haikaljavascript@gmail.com>
2024-03-16 14:17:59 +0800

    Install shadcdn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div className="text-green-500">Hello Lingo</div>
+      <Button size="lg" variant="ghost">
+        Click me
+      </Button>
     </div>
   );
 }
