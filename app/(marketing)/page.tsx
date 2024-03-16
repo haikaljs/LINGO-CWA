b292a2186f84cbb6b51d5906192cf0c397fcaa92
haikaljs <haikaljavascript@gmail.com>
2024-03-16 15:15:28 +0800

    Marketing skeleton - Header, Footer, Layout component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,11 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Button size="lg" variant="ghost">
-        Click me
-      </Button>
-    </div>
-  );
+  return <div>This is marketing page!</div>;
 }
