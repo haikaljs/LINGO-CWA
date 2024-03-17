38cf53a88d74ef39c9c8081efbbd0c198c2a680c
haikaljs <haikaljavascript@gmail.com>
2024-03-17 12:50:37 +0800

    Learn - Header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,12 +1,13 @@
 import { FeedWrapper } from "@/components/feedback-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6 ">
       <StickyWrapper>My sticky sidebar</StickyWrapper>
       <FeedWrapper>
-        
+        <Header title="Spanish"/>
       </FeedWrapper>
     </div>
   );
